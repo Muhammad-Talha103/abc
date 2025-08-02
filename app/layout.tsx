512f269d57aa049c4653de0fb3bcb6c2d16715ee
Muhammad Talha <160113245+Muhammad-Talha103@users.noreply.github.com>
2025-08-02 13:39:45 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'JSE Scanner',
+  description: 'JSE Scanner',
+  generator: 'JSE Scanner',
 }
 
 export default function RootLayout({
